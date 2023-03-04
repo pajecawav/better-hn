aab537fe10d3e84710e3624aa87acdb7f3c24226
pajecawav <pajecawav@gmail.com>
2023-03-04 20:05:34 +0300

    feat: forwards and backwards navigation hotkeys

--- src/app.vue
+++ src/app.vue
@@ -8,9 +8,14 @@
 <script setup lang="ts">
 import { useHotkeys } from "./composables/useHotkeys";
 
+const router = useRouter();
+
 const { toggleTheme } = useTheme();
 
 useHotkeys({
+	H: () => history.state.back && router.back(),
+	L: () => history.state.forward && router.forward(),
+
 	"g t": () => navigateTo("/top"),
 	"g n": () => navigateTo("/new"),
 	"g a": () => navigateTo("/ask"),
