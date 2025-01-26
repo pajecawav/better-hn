f60d4050ddc537b78c13f5154663b078403d0647
pajecawav <pajecawav@gmail.com>
2025-01-26 13:48:39 +0300

    feat: add vercel analytics

--- src/app.vue
+++ src/app.vue
@@ -3,9 +3,12 @@
 		<NuxtLoadingIndicator :color="false" />
 		<NuxtPage />
 	</NuxtLayout>
+	<Analytics />
 </template>
 
 <script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+
 const { themeColor } = useTheme();
 
 useHead({
